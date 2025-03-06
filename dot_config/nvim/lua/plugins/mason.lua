7d194e2c84eb03ca6ad1ae04cdc4c3f91dafac1c
David Stotijn <dstotijn@gmail.com>
2025-03-07 00:05:57 +0100

    Add nvim config

---
+++ dot_config/nvim/lua/plugins/mason.lua
@@ -0,0 +1,35 @@
+-- Customize Mason plugins
+
+---@type LazySpec
+return {
+  -- use mason-lspconfig to configure LSP installations
+  {
+    "williamboman/mason-lspconfig.nvim",
+    opts = {
+      ensure_installed = {
+        "gopls",
+        "pylsp",
+      },
+    },
+  },
+  -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
+  {
+    "jay-babu/mason-null-ls.nvim",
+    opts = {
+      ensure_installed = {
+        "biome",
+        "buf",
+        "delve",
+        "pyright",
+      },
+    },
+  },
+  {
+    "jay-babu/mason-nvim-dap.nvim",
+    opts = {
+      ensure_installed = {
+        "python",
+      },
+    },
+  },
+}
