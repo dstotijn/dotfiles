56e4bc83320db6dbd09a2fdc7e5601f2225cef0c
David Stotijn <dstotijn@gmail.com>
2025-03-06 14:44:12 +0100

    Use fish abbrs via config.fish

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -1,5 +1,14 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+  abbr -a -- cz chezmoi
+  abbr -a -- gpf 'git push --force-with-lease'
+  abbr -a -- gst 'git status'
+  abbr -a -- gcb 'git checkout -b'
+  abbr -a -- gd 'git diff'
+  abbr -a -- gpsup 'git push --set-upstream origin $(git branch --show-current)'
+  abbr -a -- gup 'git pull --rebase'
+  abbr -a -- gco 'git checkout'
+  abbr -a -- gp 'git push'
+  abbr -a -- gac 'git add -A && git commit'
 end
 
 source "$(brew --prefix)/share/google-cloud-sdk/path.fish.inc"
